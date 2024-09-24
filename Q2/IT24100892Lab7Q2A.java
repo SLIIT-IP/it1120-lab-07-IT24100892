d8e7a1862febe6aec99174f16c2f34af2d92e728
IT24100892 <it24100892@my.sliit.lk>
2024-09-25 05:25:52 +0530

    IT24100892Lab7Q2A

---
+++ Q2/IT24100892Lab7Q2A.java
@@ -0,0 +1,15 @@
+public class IT24100892Lab7Q2A 
+{
+    public static void main(String[] args)
+ {
+	int i, j;
+	for (j = 0; j < 5; j++) 
+	{	
+       		for ( i = 0; i < 5; i++) 
+		{
+            	System.out.print("$ ");
+        	}
+	System.out.println("");
+	}
+    }
+}
